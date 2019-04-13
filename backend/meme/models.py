b511fbc9648ea13867f150d81e2058538a2478d6
Gerard <gerwis1998@gmail.com>
2019-04-13 11:39:32 +0200

    created models

--- backend/meme/models.py
+++ backend/meme/models.py
@@ -1,13 +1,29 @@
+import json
+
 from django.db import models
+from django.utils import timezone
 
 
 class Video(models.Model):
     url = models.CharField(max_length=200)
     quality = models.FloatField()
     views = models.BigIntegerField()
+    date = models.DateTimeField(default=timezone.now)
+
+    @property
+    def to_dict(self):
+        data = {
+            'url': json.loads(self.url),
+            'date': self.date,
+            'quality': self.quality,
+            'views': self.views
+        }
+        return data
+
+    def __str__(self):
+        return self.url
 
 
 class Ranking(models.Model):
     video = models.ForeignKey(Video, on_delete=models.CASCADE)
     position = models.BigIntegerField()
-
