543ea4532b40040751e6477a3c5d15b1a50acd54
Gerard <gerwis1998@gmail.com>
2019-04-13 11:11:23 +0200

    created models

--- backend/meme/models.py
+++ backend/meme/models.py
@@ -1,1 +1,13 @@
-# Create your models here.
+from django.db import models
+
+
+class Video(models.Model):
+    url = models.CharField(max_length=200)
+    quality = models.FloatField()
+    views = models.BigIntegerField()
+
+
+class Ranking(models.Model):
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    position = models.BigIntegerField()
+
