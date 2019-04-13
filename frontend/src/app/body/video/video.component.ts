aae3cc596130b60232d5743a747a113926a6c324
Mateusz Buchajewicz <theherrlupardo@gmail.com>
2019-04-13 17:08:01 +0200

    design upgrades

--- frontend/src/app/body/video/video.component.ts
+++ frontend/src/app/body/video/video.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-video',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video.component.css']
 })
 export class VideoComponent implements OnInit {
-
-  constructor() { }
+  link;
+  base_link = "https://www.youtube.com/embed/";
+  constructor(private domSanitizer : DomSanitizer) { }
 
   ngOnInit() {
+    this.link = this.domSanitizer.bypassSecurityTrustResourceUrl(this.toYoutubeEmbedUrl("CEzSXX3tcmU"));
+  }
+
+  private toYoutubeEmbedUrl(id: string): string{
+    return this.base_link + id;
   }
 
 }
